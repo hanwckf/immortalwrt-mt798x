722aef9de1e75bff9949552a7eea306e7cf10fe7
Tianling Shen <cnsztl@immortalwrt.org>
2023-07-05 14:26:49 +0800

    mediatek: add CETRON CT3003 support (#91)
    * mediatek: add CETRON CT3003 support
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    
    * conninfra: remove incorrect default val
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    
    ---------
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -206,6 +206,23 @@ define Device/mt7981-360-t7-108M
 endef
 TARGET_DEVICES += mt7981-360-t7-108M
 
+define Device/cetron_ct3003
+  DEVICE_VENDOR := CETRON
+  DEVICE_MODEL := CT3003
+  DEVICE_DTS := mt7981-cetron-ct3003
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := cetron,ct3003
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114816k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cetron_ct3003
+
 define Device/glinet_gl-mt3000
   DEVICE_VENDOR := GL.iNet
   DEVICE_MODEL := GL-MT3000
