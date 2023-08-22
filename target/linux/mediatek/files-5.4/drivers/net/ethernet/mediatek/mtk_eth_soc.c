efef901d06e9193fc283cc601aee26c8ca59d640
developer <developer@mediatek.com>
2023-08-22 15:37:30 +0800

    [Description] Change qdma dmad from sram to dram
    [Release-log]
    -dmad in sram and payload in dram may probabilistic cause control
     path is incoherent with payload path
    -causing the actual packet sent is not the one
     you really want
    
    Change-Id: Ieeca5093f3cb24e07d659c8bec10fbaa0bbb336a
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/7912387

--- target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -3862,7 +3862,7 @@ static const struct mtk_soc_data mt7986_data = {
 	.hw_features = MTK_HW_FEATURES,
 	.required_clks = MT7986_CLKS_BITMAP,
 	.required_pctl = false,
-	.has_sram = true,
+	.has_sram = false,
 };
 
 static const struct mtk_soc_data mt7981_data = {
@@ -3871,7 +3871,7 @@ static const struct mtk_soc_data mt7981_data = {
 	.hw_features = MTK_HW_FEATURES,
 	.required_clks = MT7981_CLKS_BITMAP,
 	.required_pctl = false,
-	.has_sram = true,
+	.has_sram = false,
 };
 
 static const struct mtk_soc_data rt5350_data = {
