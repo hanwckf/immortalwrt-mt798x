1814465e22f40224ea7adc791d7c27a147b2632f
Zy143L <zy143l@foxmail.com>
2023-11-11 21:25:03 +0800

    add C8-660(WT9103) 512M Flash && fixs DTS (#155)
    * add nradio wt9103 support
    
    * add C8-660(WT9103) 512M Flash && fixs DTS

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -504,3 +504,20 @@ define Device/nradio_wt9103
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += nradio_wt9103
+
+define Device/nradio_wt9103_512m
+  DEVICE_VENDOR := NRADIO
+  DEVICE_MODEL := WT9103 (with 512M Flash)
+  DEVICE_DTS := mt7981-nradio-wt9103-512m
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := nradio,wt9103
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 131072k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += nradio_wt9103_512m
