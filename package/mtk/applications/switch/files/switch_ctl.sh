0e1af6876131ef098618636693b9f0a83ef447ea
hanwckf <hanwckf@vip.qq.com>
2023-12-19 22:56:03 +0800

    switch: add switch_ctl.init to restart mt753x ports when LAN reload

--- package/mtk/applications/switch/files/switch_ctl.sh
+++ package/mtk/applications/switch/files/switch_ctl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ ! -x /usr/sbin/switch ] && exit 1
+[ ! -x /usr/sbin/switch ] && return
 
 # power off ports
 # $1 ports list, ex: "1 2 3"
@@ -14,7 +14,7 @@ sw_poweroff_ports() {
 
 		# register 0, bit 11 is power down control bit, set to 1
 		set_value=$(($ori_value | 0x800))
-		switch phy cl22 w $p 0 $set_value
+		switch phy cl22 w $p 0 $set_value >/dev/null 2>&1
 	done
 }
 
@@ -30,7 +30,7 @@ sw_poweron_ports() {
 
 		# register 0, bit 11 is power down control bit, set to 0
 		set_value=$(($ori_value & ~0x800))
-		switch phy cl22 w $p 0 $set_value
+		switch phy cl22 w $p 0 $set_value >/dev/null 2>&1
 	done
 }
 # restart Auto-neg on ports
@@ -45,6 +45,6 @@ sw_restart_port() {
 
 		# register 0, bit 9 is Restart Auto-Negotiation bit, set to 1
 		set_value=$(($ori_value | 0x200))
-		switch phy cl22 w $p 0 $set_value
+		switch phy cl22 w $p 0 $set_value >/dev/null 2>&1
 	done
 }
