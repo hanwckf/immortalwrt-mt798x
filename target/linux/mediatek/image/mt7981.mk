78343a5fccf81ced2dbcce31f08cb38141f3e833
Aabandon <aaban_don@qq.com>
2024-08-28 11:07:44 +0800

    mediatek: add support for Newland NL-WR8103/MR3000D-CIq(256MB RAM version)
    Hardware specification:
    SoC: MediaTek MT7981B 2x A53
    Flash: FORESEE F35SQA001G 128MB
    RAM: DDR3 256MB
    Ethernet: 4x 10/100/1000 Mbps
    Switch: MediaTek MT7531AE
    WiFi: MediaTek MT7976C
    Button: Reset, Mesh
    Power: DC 12V 1A

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -667,6 +667,23 @@ define Device/nokia_ea0326gmp
 endef
 TARGET_DEVICES += nokia_ea0326gmp
 
+define Device/newland_nl-wr8103
+  DEVICE_VENDOR := Newland
+  DEVICE_MODEL := NL-WR8103
+  DEVICE_DTS := mt7981-newland-nl-wr8103
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := newland,nl-wr8103
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 116736k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += newland_nl-wr8103
+
 define Device/nradio_wt9103
   DEVICE_VENDOR := NRADIO
   DEVICE_MODEL := WT9103
