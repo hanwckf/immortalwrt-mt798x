2a6262a143526458c5e9953b1621595da453cd5b
hchyhchyxh <641953443@qq.com>
2024-09-09 17:20:43 +0800

    mediatek:add zhao-7981r128 support

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -553,3 +553,21 @@ define Device/nradio_wt9103_512m
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += nradio_wt9103_512m
+
+define Device/zhao_7981-r128
+  DEVICE_VENDOR := ZHAO
+  DEVICE_MODEL := 7981 R128
+  DEVICE_DTS := mt7981-spim-snand-7981r128
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) 
+  SUPPORTED_DEVICES := zhao,7981-r128
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 114688k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += zhao_7981-r128
